5347bde4eefbb4538aafba9047e3d250fa60a6c1
dennerv <denner.vieira@live.com>
2022-03-22 17:41:16 -0300

    Modificação pasta Home/ Adição pasta Navbar e Footer

---
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+/*import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'*/
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+
+function Navbar() {
+    return (
+        <>
+
+<AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box style={{ cursor: "pointer" }} >
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal do Denner 
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Logout
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+
+        </>
+    );
+}
+export default Navbar;
+
+/*import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';*/
+
+
+
+
