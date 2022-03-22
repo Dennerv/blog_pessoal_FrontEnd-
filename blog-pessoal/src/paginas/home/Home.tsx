eb36086daf8dd5af7067f5c4f0a92744f6e703e9
dennerv <denner.vieira@live.com>
2022-03-22 08:46:00 -0300

    iniciando em React

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Ola!!!! </h1>
+            <img src="https://i.pinimg.com/474x/9d/e0/ae/9de0ae3fab4ecc45f409e78bb5733c67.jpg" alt="imagem" className="imagem"/> 
+
+        </>
+    );
+}
+
+export default Home;
