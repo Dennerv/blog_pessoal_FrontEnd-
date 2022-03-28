b6529e47aa957eaa447024e89c9dace6088b3da0
dennerv <denner.vieira@live.com>
2022-03-28 11:48:08 -0300

    Adicionando Pag Login e Home

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,45 +1,49 @@
 import React from "react";
 /*import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core'*/
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import './Navbar.css'
+import { color } from "@mui/system";
 
 function Navbar() {
     return (
         <>
 
-<AppBar position="static">
+            <AppBar position="static">
                 <Toolbar variant="dense">
                     <Box style={{ cursor: "pointer" }} >
                         <Typography variant="h5" color="inherit">
-                            BlogPessoal do Denner 
-                        </Typography>
+                            Rafael                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                Logout
-                            </Typography>
-                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Link>
                     </Box>
 
                 </Toolbar>
